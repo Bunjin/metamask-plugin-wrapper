424de02f172ed20145c1c68df4f5824ec9a17baf
WhySoS3rious <sydhelly@hotmail.com>
2019-04-09 12:59:13 +0200

    dummy plugin interface

--- examples/dummy-remote-plugin/iframe.js
+++ examples/dummy-remote-plugin/iframe.js
@@ -1,12 +1,28 @@
+const h = React.createElement
+
+let formInput1
+
 function pluginFunction1(){
-  return pluginApi.pluginFunction1("1")
+  return pluginApi.pluginFunction1(formInput1)
 }
+function pluginFunction2(){
+  return pluginApi.pluginFunction2("1'/2", "test")
+}
+
 function renderUi(){
-  
-  const elem = React.createElement('div', null, 'Sandboxed Iframe remote Ui')
-  const elem2 = React.createElement('button', {onClick:pluginFunction1}, 'plugin function 1')  
-  console.log(elem)
-  return [elem, elem2]
+
+  const elem = h('div', null, 'Sandboxed Iframe remote Ui')
+  const elem2 = h('input', {
+	  key: "input1",
+	  className: 'inputform',
+	  placeholder: "input1",
+	  onChange: e => {
+	    formInput1 = e.target.value
+	  },
+	})  
+  const elem3 = React.createElement('button', {onClick:pluginFunction1, key:"button1"}, 'plugin function 1')
+  const elem4 = React.createElement('button', {onClick:pluginFunction2, key:"button2"}, 'plugin function 2')    
+  return [elem, elem2, elem3, elem4]
 }
 
 renderUi()
